0cfe7ef3c3c6cc154aaebc553a7ccc1e2b696007
tvc95 <thiagovc1500@gmail.com>
2021-03-09 20:30:31 -0300

    Minor changes in CSS

--- src/pages/DexData/index.tsx
+++ src/pages/DexData/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { MDBCol, MDBContainer, MDBRow } from 'mdbreact';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
@@ -7,10 +8,69 @@ import DexNavbar from '../../components/Navbars/DexNavbar/DexNavbar';
 import PokemonHeader from '../../containers/PokemonHeader/PokemonHeader';
 import PkmnArtCarousel from '../../components/Carousels/PokemonArtCarousel/PkmnArtCarousel';
 
+interface Pokemon {
+  name: string;
+  id: number;
+  order: number;
+  base_happiness: number;
+  capture_rate: number;
+  gender_rate: number;
+  hatch_counter: number;
+  forms_switchable: boolean;
+  is_baby: boolean;
+  is_legendary: boolean;
+  is_mythical: boolean;
+  egg_groups: Array<EggGroup>;
+  evolution_chain: {
+    url: string;
+  };
+  flavor_text_entries: Array<FlavorText>;
+  form_descriptions: Array<any>;
+  generation: {
+    name: string;
+  };
+  growth_rate: {
+    name: string;
+  };
+  varieties: Array<Varieties>;
+
+}
+
+interface EggGroup {
+  name: string;
+  url: string;
+}
+
+interface FlavorText {
+  flavor_text: string;
+  language: {
+    name: string;
+  };
+  version: {
+    name: string;
+  }
+}
+
+interface Varieties {
+  is_default: boolean;
+  pokemon: {
+    name: string;
+    url: string;
+  };
+}
+
+interface Ability {
+  ability: {
+    name: string;
+  }
+  is_hidden: boolean;
+}
+
 const DexData: React.FC = () => {
   const location = useLocation();
   const [pathName] = useState(location.pathname);
-  const [pkmnDexData, setPkmnDexData] = useState([]);
+  const [pkmnDexData, setPkmnDexData] = useState<Pokemon | null>(null);
+  const [genName, setGenName] = useState('');
 
   const pkmnQuery = gql`
     query pokemon {
@@ -51,27 +111,62 @@ const DexData: React.FC = () => {
     }
   `;
 
-  const { loading, data } = useQuery(pkmnQuery);
-
-  const fetchPokemonData = () => {
-    if (!loading) {
-      axios.get(data.pokemon.species.url)
-        .then((response) => {
-          if (response.status >= 200 && response.status <= 299) {
-            return response.data;
+  const megaQuery = gql`
+    query pokemon {
+      pokemon(name: "${pathName.slice(13)}-mega") {
+        name
+        types {
+          slot
+          type {
+            name
           }
-          throw Error(response.data.message);
-        })
-        .then((pkmnFullData) => {
-          setPkmnDexData(pkmnFullData);
-          console.log(pkmnDexData);
-        });
+        }
+        id
+        order
+        height
+        weight
+        abilities {
+          ability {
+            name
+          }
+          is_hidden
+        }
+        stats {
+          stat {
+            name
+          }
+          base_stat
+        }
+        forms {
+          url
+          name
+        }
+      }
     }
+  `;
+
+  const { loading, data } = useQuery(pkmnQuery);
+  // const { loadingMega, megaData } = useQuery(megaQuery);
+
+  const formatGenText = (text: string) => {
+    const [gen, number] = text.split('-');
+
+    const outputText = `${gen.charAt(0).toUpperCase() + gen.substr(1)} ${number.toUpperCase()}`;
+
+    return outputText;
   };
 
   useEffect(() => {
-    fetchPokemonData();
-  }, []);
+    async function fetchPkmnData(): Promise<void> {
+      const response = await axios.get(data.pokemon.species.url);
+
+      setPkmnDexData(response.data);
+      setGenName(formatGenText(response.data.generation.name));
+      // console.log(pkmnDexData);
+    }
+
+    fetchPkmnData();
+  }, [data.pokemon.species.url, pkmnDexData]);
 
   if (loading) {
     return (
@@ -84,7 +179,7 @@ const DexData: React.FC = () => {
   }
 
   return (
-    <div>
+    <>
       <DexNavbar />
       <PokemonHeader
         name={data.pokemon.name}
@@ -105,26 +200,45 @@ const DexData: React.FC = () => {
             <div id="description">
               <h3>Description</h3>
               <p id="flavor-txt">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Ab numquam nulla, exercitationem necessitatibus veniam explicabo quia aspernatur
-                ad inventore, voluptate soluta consequatur? Ex fugiat illum quisquam. Fugit
-                placeat quae unde.
+                {pkmnDexData?.flavor_text_entries.map((entry) => {
+                  if (entry.language.name === 'en') {
+                    if (entry.version.name === 'sword' || entry.version.name === 'shield') {
+                      return (`${entry.flavor_text} `);
+                    }
+                  }
+                  return null;
+                })}
               </p>
               <p>
                 <small>
                   <em>
-                    First introduced in Generation I (Pokémon Red/Blue/Green/Yellow)
+                    First introduced in
+                    {' '}
+                    {genName}
                   </em>
                 </small>
               </p>
             </div>
             <br />
-            <div id="a" />
+
+            <div id="abilities">
+              <h3>Abilities</h3>
+              <ul>
+                {data.pokemon.abilities.map((ability: Ability) => (
+                  <li key={ability.ability.name}>
+                    <div className={`is-hidden-${ability.is_hidden.toString()}`}>
+                      <span>{ability.ability.name}</span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+
+            </div>
           </MDBCol>
         </MDBRow>
 
       </MDBContainer>
-    </div>
+    </>
   );
 };
 
