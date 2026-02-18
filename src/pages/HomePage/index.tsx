d16cd1b7f50e075d2ee0eb272c5c35b9a87dc52d
tvc95 <thiagovc1500@gmail.com>
2026-02-17 23:38:36 -0300

    Removed unused imports

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { MDBCol, MDBContainer, MDBRow } from 'mdbreact';
+import { MDBContainer, MDBRow } from 'mdbreact';
 import {
   JumbotronLogo,
   PokedexLogo,
